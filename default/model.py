9715d8cb22defb2220a070899eb1b09958f7333b
sallieeky <sallieeky@gmail.com>
2022-10-19 07:32:16 +0800

    fix default model

--- default/model.py
+++ default/model.py
@@ -3,4 +3,4 @@ from models.model import Model
 
 class ClassName(Model):
     def __init__(self):
-        Model.__init__(self, self.__class__.__name__.lower())
+        Model.__init__(self, __class__.__name__.lower())
